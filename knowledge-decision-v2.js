a121906eaf58b26e981ca784d4f81d758b0448f4
slq520168-creator <slq520168@gmail.com>
2026-08-17 18:53:03 +0800

    Load strict hybrid search v6

--- knowledge-decision-v2.js
+++ knowledge-decision-v2.js
@@ -1,1 +1,1 @@
-(()=>{'use strict';document.write('<script src="knowledge-decision-v5.js?v=20260817-local-hybrid-1"><\/script>')})();
+(()=>{'use strict';document.write('<script src="knowledge-decision-v6.js?v=20260817-hybrid-strict-1"><\/script>')})();
