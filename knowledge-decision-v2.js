e14759f4a3cd2160a0045bddc38b02ec0858265e
slq520168-creator <slq520168@gmail.com>
2026-08-17 18:59:42 +0800

    Load local detail narrowing search v8

--- knowledge-decision-v2.js
+++ knowledge-decision-v2.js
@@ -1,1 +1,1 @@
-(()=>{'use strict';document.write('<script src="knowledge-decision-v7.js?v=20260817-local-route-compose-1"><\/script>')})();
+(()=>{'use strict';document.write('<script src="knowledge-decision-v8.js?v=20260817-local-detail-narrow-1"><\/script>')})();
